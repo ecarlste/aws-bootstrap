b81a2e15f3f7eb913f7971e00c49d8537345a809
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-09 09:39:07 -0600

    Fixed sed issue with single quotes

--- start-service.sh
+++ start-service.sh
@@ -4,16 +4,13 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
-REGION="$(
+REGION=$(
   wget -qO- http://instance-data/latest/meta-data/placement/availability-zone \
-      | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'
-)"
+      | sed -e "s:\([0-9][0-9]*\)[a-z]*\$:\\1:"
+)
 
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
-
-# Check AWS CLI version
-aws --version
 
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
 export STACK_NAME=$(
@@ -22,7 +19,5 @@ export STACK_NAME=$(
                 "Name=key,Values=aws:cloudformation:stack-name" \
       | jq -r ".Tags[0].Value"
 )
-
-echo "STACK_NAME=$STACK_NAME"
 
 npm run start 
