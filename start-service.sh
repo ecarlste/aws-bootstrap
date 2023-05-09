75d35db3a3cb30b0080dbaf810ddc5da9e58edb6
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-09 09:14:44 -0600

    Added AWS CLI version output for debug

--- start-service.sh
+++ start-service.sh
@@ -12,6 +12,9 @@ REGION="$(
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
 
+# Check AWS CLI version
+aws --version
+
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
 export STACK_NAME=$(
   aws --region "$REGION" ec2 describe-tags \
